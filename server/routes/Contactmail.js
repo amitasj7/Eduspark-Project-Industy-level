a450a0b33f5f56f730c72b3b475059c8e61a0430
Amit Sisodiya <amitsisodiyajadaun@gmail.com>
2024-12-06 02:39:21 +0530

    Delete server directory

--- server/routes/Contactmail.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { contact } = require("../controllers/Contact");
-// Define your routes
-router.post("/contact", contact);
-
-module.exports = router; // Ensure you are exporting the router correctly
